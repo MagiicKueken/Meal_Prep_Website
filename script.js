e0404fbccb677c35274051e20087ed3d03948822
MagiicKueken <charlotte@heinzelnet.de>
2025-03-25 14:06:18 +0100

    added properties to the webpage and edited the JSON-model

--- script.js
+++ script.js
@@ -1,262 +1,86 @@
 var data = {
-    "Wochentage": [
+  "Wochentage": [
       {
-        "Tag": "Montag",
-        "Mahlzeiten": [
-          {
-            "Name": "Frühstück",
-            "Gericht": "Overnight Oats mit Erdnussbutter & Beeren",
-            "Zutaten": {
-              "Haferflocken": "50 g",
-              "Milch": "200 ml",
-              "Erdnussbutter": "1 TL",
-              "Beeren": "50 g",
-              "Honig": "1 TL"
-            },
-            "Nährwerte": {
-              "Kcal": 450,
-              "Protein": 24,
-              "KH": 50,
-              "Fett": 15
-            }
-          },
-          {
-            "Name": "Abendessen",
-            "Gericht": "Hähnchenpfanne mit Brokkoli & Süßkartoffeln",
-            "Zutaten": {
-              "Hähnchenbrust": "300 g",
-              "Brokkoli": "300 g",
-              "Süßkartoffeln": "300 g",
-              "Olivenöl": "1 EL",
-              "Salz, Pfeffer": "nach Geschmack"
-            },
-            "Nährwerte": {
-              "Kcal": 520,
-              "Protein": 55,
-              "KH": 45,
-              "Fett": 12
-            }
-          }
-        ]
-      },
-      {
-        "Tag": "Dienstag",
-        "Mahlzeiten": [
-          {
-            "Name": "Frühstück",
-            "Gericht": "Griechischer Joghurt mit Mandeln & Honig",
-            "Zutaten": {
-              "Griechischer Joghurt (10%)": "200 g",
-              "Mandeln": "10 g",
-              "Honig": "1 TL"
-            },
-            "Nährwerte": {
-              "Kcal": 380,
-              "Protein": 30,
-              "KH": 20,
-              "Fett": 15
-            }
-          },
-          {
-            "Name": "Abendessen",
-            "Gericht": "Rindersteak mit Ofengemüse",
-            "Zutaten": {
-              "Rindersteak": "150 g",
-              "Gemüse (Paprika, Zucchini, Möhren)": "300 g",
-              "Olivenöl": "1 EL",
-              "Salz, Pfeffer": "nach Geschmack"
-            },
-            "Nährwerte": {
-              "Kcal": 600,
-              "Protein": 65,
-              "KH": 35,
-              "Fett": 20
-            }
-          }
-        ]
-      },
-      {
-        "Tag": "Mittwoch",
-        "Mahlzeiten": [
-          {
-            "Name": "Frühstück",
-            "Gericht": "Rührei mit Vollkornbrot",
-            "Zutaten": {
-              "Eier": "3 Stück",
-              "Vollkornbrot": "1 Scheibe",
-              "Butter": "1 TL",
-              "Salz, Pfeffer": "nach Geschmack"
-            },
-            "Nährwerte": {
-              "Kcal": 450,
-              "Protein": 35,
-              "KH": 30,
-              "Fett": 20
-            }
-          },
-          {
-            "Name": "Abendessen",
-            "Gericht": "Lachs mit Quinoa und Spinat",
-            "Zutaten": {
-              "Lachsfilet": "200 g",
-              "Quinoa": "100 g",
-              "Spinat": "200 g",
-              "Olivenöl": "1 EL"
-            },
-            "Nährwerte": {
-              "Kcal": 620,
-              "Protein": 55,
-              "KH": 45,
-              "Fett": 25
-            }
-          }
-        ]
-      },
-      {
-        "Tag": "Donnerstag",
-        "Mahlzeiten": [
-          {
-            "Name": "Frühstück",
-            "Gericht": "Proteinshake mit Banane",
-            "Zutaten": {
-              "Proteinpulver": "30 g",
-              "Banane": "1 Stück",
-              "Milch": "250 ml"
-            },
-            "Nährwerte": {
-              "Kcal": 350,
-              "Protein": 35,
-              "KH": 40,
-              "Fett": 5
-            }
-          },
-          {
-            "Name": "Abendessen",
-            "Gericht": "Hähnchen mit Reis & Gemüse",
-            "Zutaten": {
-              "Hähnchenbrust": "200 g",
-              "Reis": "100 g",
-              "Gemüse (Paprika, Karotten, Erbsen)": "200 g"
-            },
-            "Nährwerte": {
-              "Kcal": 550,
-              "Protein": 50,
-              "KH": 55,
-              "Fett": 10
-            }
-          }
-        ]
-      },
-      {
-        "Tag": "Freitag",
-        "Mahlzeiten": [
-          {
-            "Name": "Frühstück",
-            "Gericht": "Haferflocken mit Apfel & Zimt",
-            "Zutaten": {
-              "Haferflocken": "50 g",
-              "Milch": "200 ml",
-              "Apfel": "1 Stück",
-              "Zimt": "1 TL"
-            },
-            "Nährwerte": {
-              "Kcal": 400,
-              "Protein": 20,
-              "KH": 60,
-              "Fett": 10
-            }
-          },
-          {
-            "Name": "Abendessen",
-            "Gericht": "Rinderhack mit Kartoffelpüree & Bohnen",
-            "Zutaten": {
-              "Rinderhack": "200 g",
-              "Kartoffeln": "300 g",
-              "Bohnen": "200 g"
-            },
-            "Nährwerte": {
-              "Kcal": 600,
-              "Protein": 50,
-              "KH": 60,
-              "Fett": 15
-            }
-          }
-        ]
-      },
-      {
-        "Tag": "Samstag",
-        "Mahlzeiten": [
-          {
-            "Name": "Frühstück",
-            "Gericht": "Omelett mit Gemüse",
-            "Zutaten": {
-              "Eier": "3 Stück",
-              "Gemüse (Paprika, Zwiebeln, Spinat)": "100 g",
-              "Olivenöl": "1 TL"
-            },
-            "Nährwerte": {
-              "Kcal": 400,
-              "Protein": 35,
-              "KH": 15,
-              "Fett": 20
-            }
-          },
-          {
-            "Name": "Abendessen",
-            "Gericht": "Thunfischsalat mit Kichererbsen",
-            "Zutaten": {
-              "Thunfisch": "150 g",
-              "Kichererbsen": "100 g",
-              "Olivenöl": "1 EL"
-            },
-            "Nährwerte": {
-              "Kcal": 500,
-              "Protein": 50,
-              "KH": 40,
-              "Fett": 15
-            }
-          }
-        ]
-      },
-      {
-        "Tag": "Sonntag",
-        "Mahlzeiten": [
-          {
-            "Name": "Frühstück",
-            "Gericht": "Skyr mit Beeren & Walnüssen",
-            "Zutaten": {
-              "Skyr": "200 g",
-              "Beeren": "50 g",
-              "Walnüsse": "10 g"
-            },
-            "Nährwerte": {
-              "Kcal": 350,
-              "Protein": 40,
-              "KH": 25,
-              "Fett": 10
-            }
-          },
-          {
-            "Name": "Abendessen",
-            "Gericht": "Garnelen mit Quinoa & Gemüse",
-            "Zutaten": {
-              "Garnelen": "200 g",
-              "Quinoa": "100 g",
-              "Gemüse": "200 g"
-            },
-            "Nährwerte": {
-              "Kcal": 580,
-              "Protein": 55,
-              "KH": 50,
-              "Fett": 12
-            }
-          }
-        ]
+          "Tag": "Montag",
+          "Mahlzeiten": [
+              {
+                  "Name": "Frühstück",
+                  "Gericht": "Overnight Oats mit Erdnussbutter & Beeren",
+                  "Zutaten": [
+                      [
+                          "Haferflocken",
+                          "50g"
+                      ],
+                      [
+                          "Milch",
+                          "200 ml"
+                      ],
+                      [
+                          "Erdnussbutter",
+                          "1 TL"
+                      ],
+                      [
+                          "Beeren",
+                          "50 g"
+                      ],
+                      [
+                          "Honig",
+                          "1 TL"
+                      ]
+                  ],
+                  "Zubereitung": [
+                      [
+                          1,
+                          "Bring water to boil"
+                      ],
+                      [
+                          2,
+                          "Put noodles in"
+                      ]
+                  ],
+                  "Nährwerte": {
+                      "Kcal": 450,
+                      "Protein": 24,
+                      "KH": 50,
+                      "Fett": 15
+                  }
+              },
+              {
+                  "Name": "Abendessen",
+                  "Gericht": "Hähnchenpfanne mit Brokkoli & Süßkartoffeln",
+                  "Zutaten": {
+                      "Hähnchenbrust": "300 g",
+                      "Brokkoli": "300 g",
+                      "Süßkartoffeln": "300 g",
+                      "Olivenöl": "1 EL",
+                      "Salz, Pfeffer": "nach Geschmack"
+                  },
+                  "Nährwerte": {
+                      "Kcal": 520,
+                      "Protein": 55,
+                      "KH": 45,
+                      "Fett": 12
+                  }
+              }
+          ]
       }
-    ]
-  }
+  ]
+}
   
-document.getElementById("header").innerHTML = data.Wochentage[1].Tag;
-document.getElementById("meal-left-timeOfDay").innerHTML = data.Wochentage[1].Mahlzeiten[0].Name;
+let DayOfWeek = 0;
+console.log(data);
 
-document.getElementById("meal-left-recipe-name").innerHTML = data.Wochentage[1].Mahlzeiten[0].Gericht;
+document.getElementById("header").innerHTML = data.Wochentage[DayOfWeek].Tag;
+document.getElementById("meal-left-timeOfDay").innerHTML = data.Wochentage[DayOfWeek].Mahlzeiten[0].Name;
+
+document.getElementById("meal-left-recipe-name").innerHTML = data.Wochentage[DayOfWeek].Mahlzeiten[0].Gericht;
+
+let zutaten = data.Wochentage[DayOfWeek].Mahlzeiten[0].Zutaten[0][1] + " " + data.Wochentage[DayOfWeek].Mahlzeiten[0].Zutaten[0][0]
+
+document.getElementById("meal-left-ingredients").innerHTML = zutaten;
+
+let zubereitung = data.Wochentage[DayOfWeek].Mahlzeiten[0].Zubereitung[0][0] + ". " + data.Wochentage[DayOfWeek].Mahlzeiten[0].Zubereitung[0][1]
+
+document.getElementById("meal-left-preperations").innerHTML = zubereitung;
+
+
