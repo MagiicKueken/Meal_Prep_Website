c3fb690bfcefac010cce19fca6eb451ad67f460e
MagiicKueken <charlotte@heinzelnet.de>
2025-04-13 01:29:10 +0200

    made the sidebar functional

--- script.js
+++ script.js
@@ -1,72 +1,195 @@
-var initialDayData = {
-    "Dates": [
-      {
-        "Datum": "01.04.2025",
-        "Tag": "Dienstag",
-        "Mahlzeiten": [
-          {
-            "Name": "Abendessen",
-            "Gericht": "Rindersteak mit Ofengemüse",
-            "Zutaten": [
-              ["2", "Rindersteaks (je 150 g)"],
-              ["300 g", "Gemüse (Paprika, Zucchini, Möhren)"],
-              ["1 EL", "Olivenöl"],
-              ["Salz", "Pfeffer"]
-            ],
-            "Zubereitung": [
-              ["1", "Den Backofen auf 200 °C (Ober-/Unterhitze) vorheizen."],
-              ["2", "Das Gemüse waschen, in mundgerechte Stücke schneiden und mit Olivenöl, Salz und Pfeffer vermengen."],
-              ["3", "Das Gemüse auf einem Backblech verteilen und ca. 20 Minuten im Ofen garen."],
-              ["4", "Währenddessen das Steak mit Salz und Pfeffer würzen."],
-              ["5", "Eine Pfanne stark erhitzen und das Steak je nach gewünschtem Gargrad ca. 3 Minuten pro Seite braten."],
-              ["6", "Das Steak mit dem Ofengemüse servieren."]
-            ],
-            "Nährwerte": {
-              "Kcal": 600,
-              "Protein": 65,
-              "Kohlenhydrate": 35,
-              "Fett": 20
-            }
+var data = {
+  "Dates": [
+    {
+      "Datum": "2025-04-14",
+      "Tag": "Montag",
+      "Mahlzeiten": [
+        {
+          "Name": "Mittagessen",
+          "Gericht": "Hähnchenbrust mit Süßkartoffeln und Brokkoli",
+          "Zutaten": [
+            ["2", "Hähnchenbrustfilets (je 150 g)"],
+            ["2", "große Süßkartoffeln"],
+            ["300 g", "Brokkoli"],
+            ["2 EL", "Olivenöl"],
+            ["1 TL", "Paprikapulver"],
+            ["nach Geschmack", "Salz und Pfeffer"]
+          ],
+          "Zubereitung": [
+            ["1", "Den Backofen auf 200 °C (Ober-/Unterhitze) vorheizen."],
+            ["2", "Süßkartoffeln schälen, würfeln, mit 1 EL Olivenöl und Paprika würzen und im Ofen 25 Minuten backen."],
+            ["3", "Hähnchenbrust salzen, pfeffern und in 1 EL Öl in der Pfanne braten (je Seite ca. 5–6 Minuten)."],
+            ["4", "Brokkoli in Röschen schneiden und 5 Minuten dämpfen."],
+            ["5", "Alles zusammen auf einem Teller anrichten und servieren."]
+          ],
+          "Nährwerte": {
+            "Kcal": 580,
+            "Protein": 50,
+            "Kohlenhydrate": 35,
+            "Fett": 22
           }
-        ],
-        "Nährwerte": {
-          "Kcal": 600,
-          "Protein": 65,
-          "Kohlenhydrate": 35,
-          "Fett": 20
         }
-      }
-    ]
-  }
+      ]
+    },
+    {
+      "Datum": "2025-04-15",
+      "Tag": "Dienstag",
+      "Mahlzeiten": [
+        {
+          "Name": "Mittagessen",
+          "Gericht": "Lachsfilet mit Quinoa und grünem Spargel",
+          "Zutaten": [
+            ["2", "Lachsfilets (je 150 g)"],
+            ["150 g", "Quinoa"],
+            ["250 g", "grüner Spargel"],
+            ["1 EL", "Zitronensaft"],
+            ["2 EL", "Olivenöl"],
+            ["nach Geschmack", "Salz und Pfeffer"]
+          ],
+          "Zubereitung": [
+            ["1", "Quinoa gründlich waschen und nach Packungsanleitung in Salzwasser kochen."],
+            ["2", "Spargel waschen, die holzigen Enden abschneiden und in 1 EL Öl ca. 8 Minuten anbraten."],
+            ["3", "Lachsfilets mit Salz, Pfeffer und Zitronensaft würzen und in 1 EL Öl von beiden Seiten je 4–5 Minuten braten."],
+            ["4", "Lachs mit Quinoa und Spargel servieren."]
+          ],
+          "Nährwerte": {
+            "Kcal": 610,
+            "Protein": 45,
+            "Kohlenhydrate": 30,
+            "Fett": 35
+          }
+        }
+      ]
+    },
+    {
+      "Datum": "2025-04-16",
+      "Tag": "Mittwoch",
+      "Mahlzeiten": [
+        {
+          "Name": "Mittagessen",
+          "Gericht": "Vegetarische Lasagne mit Zucchini und Spinat",
+          "Zutaten": [
+            ["6", "Lasagneplatten (Vollkorn)"],
+            ["1", "Zucchini"],
+            ["150 g", "frischer Spinat"],
+            ["250 g", "passierte Tomaten"],
+            ["1", "Zwiebel"],
+            ["1", "Knoblauchzehe"],
+            ["150 g", "Mozzarella"],
+            ["1 EL", "Olivenöl"],
+            ["nach Geschmack", "Salz, Pfeffer, Oregano"]
+          ],
+          "Zubereitung": [
+            ["1", "Backofen auf 180 °C vorheizen."],
+            ["2", "Zwiebel und Knoblauch fein hacken, in Öl anbraten."],
+            ["3", "Zucchini in Scheiben schneiden und mit Spinat hinzufügen, kurz dünsten."],
+            ["4", "Passierte Tomaten und Gewürze zugeben, 10 Minuten köcheln lassen."],
+            ["5", "In eine Auflaufform abwechselnd Lasagneplatten, Gemüsesoße und Mozzarella schichten."],
+            ["6", "Für 30 Minuten im Ofen backen."]
+          ],
+          "Nährwerte": {
+            "Kcal": 540,
+            "Protein": 28,
+            "Kohlenhydrate": 45,
+            "Fett": 25
+          }
+        }
+      ]
+    },
+    {
+      "Datum": "2025-04-17",
+      "Tag": "Donnerstag",
+      "Mahlzeiten": [
+        {
+          "Name": "Mittagessen",
+          "Gericht": "Putenstreifen mit Paprika-Reis",
+          "Zutaten": [
+            ["300 g", "Putenbrustfilet"],
+            ["1", "rote Paprika"],
+            ["1", "gelbe Paprika"],
+            ["150 g", "Reis"],
+            ["1 EL", "Sojasauce"],
+            ["1 EL", "Rapsöl"],
+            ["nach Geschmack", "Salz und Pfeffer"]
+          ],
+          "Zubereitung": [
+            ["1", "Reis in Salzwasser kochen."],
+            ["2", "Paprika in Streifen schneiden, Pute würfeln."],
+            ["3", "Putenwürfel in Öl anbraten, dann Paprika zugeben und mit Sojasauce würzen."],
+            ["4", "Mit dem gekochten Reis servieren."]
+          ],
+          "Nährwerte": {
+            "Kcal": 580,
+            "Protein": 42,
+            "Kohlenhydrate": 50,
+            "Fett": 18
+          }
+        }
+      ]
+    },
+    {
+      "Datum": "2025-04-18",
+      "Tag": "Freitag",
+      "Mahlzeiten": [
+        {
+          "Name": "Mittagessen",
+          "Gericht": "Kartoffel-Gemüse-Pfanne mit Ei",
+          "Zutaten": [
+            ["300 g", "Kartoffeln"],
+            ["1", "Karotte"],
+            ["100 g", "Zucchini"],
+            ["100 g", "Brokkoli"],
+            ["2", "Eier"],
+            ["1 EL", "Rapsöl"],
+            ["nach Geschmack", "Salz, Pfeffer, Paprika"]
+          ],
+          "Zubereitung": [
+            ["1", "Kartoffeln schälen, würfeln und in Salzwasser 10 Minuten vorkochen."],
+            ["2", "Gemüse klein schneiden."],
+            ["3", "Öl in der Pfanne erhitzen, Kartoffeln und Gemüse anbraten."],
+            ["4", "Eier darüber schlagen, stocken lassen und würzen."],
+            ["5", "Heiß servieren."]
+          ],
+          "Nährwerte": {
+            "Kcal": 500,
+            "Protein": 22,
+            "Kohlenhydrate": 45,
+            "Fett": 25
+          }
+        }
+      ]
+    }
+  ]
+}
 
-displayMeal(initialDayData);
+let currentDayOfWeek = 1;
+displayMeal(data.Dates[currentDayOfWeek]);
 
 //Function for the JSON inputfield and changing data
 function openForm(){
   let jsonString = prompt("Enter new JSON data:");
   
   let jsonObject = JSON.parse(jsonString);
-  displayMeal(jsonObject);
+  data = jsonObject;
+  displayMeal(data.Dates[0]);
 }
 
-function displayMeal(data){
+function displayMeal(meal){
+let currentMeal = 0;
 
-let currentDayOfWeek = 0;
-let currentMeals = 0;
-
-console.log(data);
+console.log(meal);
 //Header
-document.getElementById("weekday").innerHTML = data.Dates[currentDayOfWeek].Tag;
-document.getElementById("meal-dateofDay").innerHTML = data.Dates[currentDayOfWeek].Datum;
+document.getElementById("weekday").innerHTML = meal.Tag;
+document.getElementById("meal-dateofDay").innerHTML = meal.Datum;
 
 //Time of day
-document.getElementById("meal-timeOfDay").innerHTML = data.Dates[currentDayOfWeek].Mahlzeiten[currentMeals].Name;
+document.getElementById("meal-timeOfDay").innerHTML = meal.Mahlzeiten[currentMeal].Name;
 
 //Recipe
-document.getElementById("meal-recipe-name").innerHTML = data.Dates[currentDayOfWeek].Mahlzeiten[currentMeals].Gericht;
+document.getElementById("meal-recipe-name").innerHTML = meal.Mahlzeiten[currentMeal].Gericht;
 
 //Ingredients aka zutaten
-let ingredients = data.Dates[currentDayOfWeek].Mahlzeiten[currentMeals].Zutaten;
+let ingredients = meal.Mahlzeiten[currentMeal].Zutaten;
 let ingredientsHtml = document.getElementById("meal-ingredients");
 ingredientsHtml.innerHTML = "Zutaten: </br>";
 ingredientsHtml.innerHTML += "<ul>";
@@ -82,7 +205,7 @@ ingredientsHtml.innerHTML += "</ul> </br></br>"
 
 //Preperations (also loop)
 
-let preperation = data.Dates[currentDayOfWeek].Mahlzeiten[currentMeals].Zubereitung;
+let preperation = meal.Mahlzeiten[currentMeal].Zubereitung;
 let preperationsHtml = document.getElementById("meal-preperations");
 
 preperationsHtml.innerHTML = "Zubereitung: </br>";
@@ -92,16 +215,19 @@ for (let i = 0; i < preperation.length; i++){
   let currentStep = preperation [i];
   preperationsHtml.innerHTML += '<li type="1">' + currentStep[1] + "</li>";
 }
-
 preperationsHtml.innerHTML += "</ol>";
 }
 
 //Sidebar
 function openfunction(){
-  document.getElementById("sidenav").style.width="250px";
-  document.getElementById("sidenav").style.transition="width 0.5s";
+  document.getElementById("sidenav").style.width="225px";
+  document.getElementById("sidenav").style.transition="width 1s";
 }
 
 function closefunction(){
   document.getElementById("sidenav").style.width="0px";
+}
+
+function selectDay(day){
+  displayMeal(data.Dates[day]);
 }
